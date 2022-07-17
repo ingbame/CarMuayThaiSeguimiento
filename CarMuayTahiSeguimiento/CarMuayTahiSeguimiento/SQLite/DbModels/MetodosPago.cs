c6bbf192d19bfa41db875df969cbed84690172d0
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-07-17 18:06:05 -0500

    Carga incial del proyecto

---
+++ CarMuayTahiSeguimiento/CarMuayTahiSeguimiento/SQLite/DbModels/MetodosPago.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace CarMuayTahiSeguimiento.SQLite.DbModels
+{
+    public class MetodosPago
+    {
+        public int MedioPagoId { get; set; }
+        public string MedioDescripcion { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<PagosPor15Clases> _PagosPor15Clases { get; set; }
+    }
+}
